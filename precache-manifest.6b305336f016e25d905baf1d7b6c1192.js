3c028188a4644101b5203ce1765b0a9b22e3ad01
Pritz96 <pritam1996@hotmail.co.uk>
2020-03-24 09:48:18 +0000

    Updates

---
+++ precache-manifest.6b305336f016e25d905baf1d7b6c1192.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46dd7dc30caa0c07e2deb5d8bca6c433",
+    "url": "/index.html"
+  },
+  {
+    "revision": "948a881a2a4dda2b50a9",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b09740bfc0c66ec64240",
+    "url": "/static/js/2.36cc523a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.36cc523a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "948a881a2a4dda2b50a9",
+    "url": "/static/js/main.aa7c29c3.chunk.js"
+  },
+  {
+    "revision": "debd0c6e6cb3d423cae3",
+    "url": "/static/js/runtime-main.5d2c196c.js"
+  },
+  {
+    "revision": "9d2029a3f4f504bf7d24259e9eb297e3",
+    "url": "/static/media/blueRing.9d2029a3.png"
+  },
+  {
+    "revision": "8fde5954c3204ffda59345b0d472637c",
+    "url": "/static/media/controllerCoaster.8fde5954.png"
+  },
+  {
+    "revision": "73fc2b9433f3d89b6a505dee3c8a4b6e",
+    "url": "/static/media/inceptionTotem.73fc2b94.png"
+  }
+]);
