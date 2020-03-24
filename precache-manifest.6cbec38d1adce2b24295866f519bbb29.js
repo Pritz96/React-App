9134573edb135cfbce107467efbd61b74c271a08
Pritz96 <pritam1996@hotmail.co.uk>
2020-03-24 10:33:29 +0000

    Updates

---
+++ precache-manifest.6cbec38d1adce2b24295866f519bbb29.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfacd3f2751523cee3adec95fc1a5af9",
+    "url": "/React-App/index.html"
+  },
+  {
+    "revision": "34db408b7122ba5c0f11",
+    "url": "/React-App/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b09740bfc0c66ec64240",
+    "url": "/React-App/static/js/2.36cc523a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/React-App/static/js/2.36cc523a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34db408b7122ba5c0f11",
+    "url": "/React-App/static/js/main.8a04138b.chunk.js"
+  },
+  {
+    "revision": "3857cece8260ff5cf3f2",
+    "url": "/React-App/static/js/runtime-main.b39ae8e8.js"
+  },
+  {
+    "revision": "9d2029a3f4f504bf7d24259e9eb297e3",
+    "url": "/React-App/static/media/blueRing.9d2029a3.png"
+  },
+  {
+    "revision": "8fde5954c3204ffda59345b0d472637c",
+    "url": "/React-App/static/media/controllerCoaster.8fde5954.png"
+  },
+  {
+    "revision": "73fc2b9433f3d89b6a505dee3c8a4b6e",
+    "url": "/React-App/static/media/inceptionTotem.73fc2b94.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6b305336f016e25d905baf1d7b6c1192.js"
+  "/React-App/precache-manifest.6cbec38d1adce2b24295866f519bbb29.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-App/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
